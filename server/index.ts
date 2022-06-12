5be2cf9ae8655f3b89e538e2cc10823ffe2ea819
Aidan Dunlap <10292904+aiddun@users.noreply.github.com>
2022-06-12 01:07:19 -0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ const port = 3000
 import algosdk from "algosdk";
 const Database = require("@replit/database")
 var cors = require('cors')
+import nearAPI from "near-api-js";
 
 
 const db = new Database()
@@ -14,60 +15,61 @@ app.use(cors())
 app.use(express.json());
 
 
-const token = {
-    'X-API-Key': process.env.APIKEY
-}
-const algodServer = "https://testnet-algorand.api.purestake.io/ps2";
-const aport = '';
+const keyPair = nearAPI.KeyPair.fromString(process.env.NEAR_STRING)
+const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+keyStore.setKey('testnet', 'aidan.testnet', keyPair);
+
+const config = {
+  keyStore, // instance of InMemoryKeyStore
+  networkId: 'testnet',
+  nodeUrl: 'https://rpc.testnet.near.org',
+  walletUrl: 'https://wallet.testnet.near.org',
+  helperUrl: 'https://helper.testnet.near.org',
+  explorerUrl: 'https://explorer.testnet.near.org'
+};
+
+// inside an async function
+let near;
 
 
-const algodClient = new algosdk.Algodv2(token, algodServer, aport);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/api/tap', async (req, res) => {
+
+
+app.post('/api/near', async (req, res) => {
   const body = req.body;
   const { address } = body;
 
-  if ((await db.get(address)) === true) 
-  {
+  console.log(body)
+
+  if (address == null) {
+    res.send(400)
+    return
+  }
+  
+  if ((await db.get(address)) === true) {
     res.send({ "error": "already claimed" })
     return
-  } 
+  }
 
-  let params = await algodClient.getTransactionParams().do();
-  // comment out the next two lines to use suggested fee
-  params.fee = 1000;
-  params.flatFee = true;
+  if (!near) {
+    near = await nearAPI.connect(config);
+  }
 
-  const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-    from: "YCVKCMTPENADSHKA3I24SPDRT5JHLYAUZZ36W22J3K7OR62FDWPOBXQDUY",
-    to: address,
-    amount: 1,
-    suggestedParams: params
-  });
+  const account = await near.account("aidan.testnet");
+  const resp = await account.sendMoney(
+    address, // receiver account
+    "1000" // amount in yoctoNEAR
+  );
 
-
-  let account = algosdk.mnemonicToSecretKey(process.env.PHRASE);
-  let signedTxn = txn.signTxn(account.sk);
-
-  // send signed transaction to node
-  let tx = await algodClient.sendRawTransaction(signedTxn).do();
-  console.log("Signed transaction with txID: %s", tx.txId);
-  // Wait for confirmation
-  let confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4);
-  //Get the completed Transaction
-  console.log("Transaction " + tx.txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
-  let mytxinfo = JSON.stringify(confirmedTxn.txn.txn, undefined, 2);
-  console.log("Transaction information: %o", mytxinfo);
-  var string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
-  console.log("Note field: ", string);
+  console.log(resp.transaction_outcome)
 
   db.set(address, true)
-
-  res.send({ txid: tx.txId })
+  res.send({id: resp.transaction_outcome.id})
 })
 
 app.listen(port, () => {
